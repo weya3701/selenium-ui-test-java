fe410bda10a25f82b075b0b447b5cfa9cd6f6648
Allen Yang <musasi.yang@gmail.com>
2023-07-22 13:15:37 +0800

    refactor: 重構task parser功能
    改寫yaml開檔方式，讀取檔案內容並輸出為字串，再由yaml解析器解析。

--- src/main/java/org/example/TaskParser.java
+++ src/main/java/org/example/TaskParser.java
@@ -9,8 +9,30 @@ public class TaskParser {
 
     private TestJob testJob;
 
+    public String fileParser(String filePath) {
+        String fileContent = null;
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+
+            bufferedReader.close();
+
+            fileContent = stringBuilder.toString();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return fileContent;
+    }
+
     public TaskParser(String yamlFile) {
-        System.out.println(yamlFile);
 
         Representer representer = new Representer(new DumperOptions());
         representer.getPropertyUtils().setSkipMissingProperties(true);
@@ -19,33 +41,9 @@ public class TaskParser {
             Yaml yaml = new Yaml(representer);
 
             StringBuffer sb=new StringBuffer();
-            File file=new File("/Users/mirage/Desktop/yuantaDemo.yaml");
-            System.out.println(file.exists());
-            FileInputStream inputStream = new FileInputStream(file);
-            InputStreamReader isr=new InputStreamReader(inputStream);
-            BufferedReader br=new BufferedReader(isr, 409600);
-            try {
-                while (true) {
-                    String line = null;
-                    try {
-                        line = br.readLine();
-                        if(line==null) break;
-                        System.out.println(line);
-                    } catch (IOException e) {
-                        break;
-                    }
-                    if(line.indexOf("#")!=-1) continue;
-                    sb.append(line).append("\n");
-                }
-            }catch(Exception ex) {
-                ex.printStackTrace();
-            }
-            finally {
-                try { br.close(); }catch(Exception exx) {}
-                try { isr.close(); }catch(Exception exx) {}
-                try { inputStream.close(); }catch(Exception exx) {}
-            }
-            TestJob testJob = yaml.loadAs(sb.toString(), TestJob.class);
+            String yamlOutput = fileParser(yamlFile);
+
+            TestJob testJob = yaml.loadAs(yamlOutput, TestJob.class);
             this.testJob = testJob;
         } catch (Exception ex) {
             System.out.println(ex.toString());
