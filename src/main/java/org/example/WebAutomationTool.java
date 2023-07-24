0de5aea97ea0945cb672d7d54cf99885f05cf01d
Allen Yang <musasi.yang@gmail.com>
2023-07-24 15:35:01 +0800

    refactor: 新增webdriver介面方法
    新增WebAutomationTool介面方法，
    switch_tab, open_new_tab, open_element_and_click_without_wait,
    find_element_and_sendkey, find_element_and_sendkey_from_store,
    switch_frame, get_value_to_store, get_regex_value_to_store,
    validation_count

--- src/main/java/org/example/WebAutomationTool.java
+++ src/main/java/org/example/WebAutomationTool.java
@@ -1,11 +1,28 @@
 package org.example;
 
 public interface WebAutomationTool {
+  static final String SUCCESSFUL = "執行成功";
+  static final String FAILED = "執行失敗";
 
-    static final String SUCCESSFUL = "執行成功";
-    static final String FAILED = "執行失敗";
+  public void open_website(Step step);
 
-    public void open_website(Step step);
+  public void find_element_and_click(Step step);
 
-    public void find_element_and_click(Step step);
+  public void switch_tab(Step step);
+
+  public void open_new_tab(Step step);
+
+  public void open_element_and_click_without_wait(Step step);
+
+  public void find_element_and_sendkey(Step step);
+
+  public void find_element_and_sendkey_from_store(Step step);
+
+  public void switch_frame(Step step);
+
+  public void get_value_to_store(Step step);
+
+  public void get_regex_value_to_store(Step step);
+
+  public void validation_count(Step step);
 }
