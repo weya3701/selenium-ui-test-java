52a45bf85c494f07617db8b8903a161dbe7ba414
Allen Yang <musasi.yang@gmail.com>
2023-07-31 10:41:01 +0800

    refactor: 重構TaskRunner執行測案步驟方式 TaskRunner啟動方式設定來源皆由測案設定檔引入，增加程式操作彈性。

--- src/main/java/org/example/TaskRunner.java
+++ src/main/java/org/example/TaskRunner.java
@@ -1,26 +1,22 @@
 package org.example;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TaskRunner {
-    public TaskRunner(Step[] steps) {
+    public TaskRunner(TestJob testJob) {
 
         Class c = DemoWebDriver.class;
-
         Constructor con = c.getDeclaredConstructors()[0];
 
-        // 取得Webdriver運行實體
         try {
-            Object wb = con.newInstance("Chrome", "/Users/mirage/Desktop/chromedriver");
-            // Object wb = con.newInstance(wdDriver, wdPath);
-            for (Step step: steps) {
-                // 取得物件方法
-                Method method = c.getMethod(step.module, Step.class);
-                // 執行方法
+            Object wb = con.newInstance(testJob.getWebdriverType(), testJob.getWebdriverPath(), testJob.getOptions());
+            for (Step step: Arrays.stream(testJob.steps).toList()) {
+                Method method = c.getMethod(step.module, step.getClass());
                 method.invoke(wb, step);
             }
         } catch (Exception ex) {
-            System.out.println(ex.toString());
+            ex.printStackTrace();
         }
 
     }
