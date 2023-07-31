05f1fad47daad0cabd5d0b97e7dadfa01e11cd04
Allen Yang <musasi.yang@gmail.com>
2023-07-31 10:38:07 +0800

    refactor: 重構Webdriver啟動方式 新增Webdriver啟動方式，將原本僅傳入步驟改為傳入完整測案設定資料。

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,6 @@
 package org.example;
 import java.lang.reflect.Constructor;
-import org.yaml.snakeyaml.Yaml;
-// import org.yaml.snakeyaml.constructor.Constructor;
-import org.yaml.snakeyaml.representer.Representer;
-import java.io.InputStream;
-import java.io.FileInputStream;
-import java.lang.reflect.Method;
-
-import java.io.InputStream;
-import java.io.FileInputStream;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,10 +8,8 @@ public class Main {
 
         Constructor con = c.getDeclaredConstructors()[0];
 
-        System.out.println(args[0]);
-        // TaskParser tp = new TaskParser("/Users/mirage/Desktop/yuantaDemo.yaml");
         TaskParser tp = new TaskParser(args[0]);
         TestJob tj = tp.getTestJob();
-        new TaskRunner(tj.steps);
+        new TaskRunner(tj);
     }
 }
