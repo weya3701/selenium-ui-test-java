a2661ef02c484b94f89e7efd2e421ee438374aea
Allen Yang <musasi.yang@gmail.com>
2023-08-07 18:21:20 +0800

    feature: 新曾報告markdown格式輸出， 驗證報告輸出結果，輸出結果為Markdown格式。

--- src/main/java/org/example/TaskRunner.java
+++ src/main/java/org/example/TaskRunner.java
@@ -3,6 +3,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class TaskRunner {
     public TaskRunner(TestJob testJob) {
 
@@ -18,9 +21,26 @@ public class TaskRunner {
                 saveMethod.invoke(wb, step.desc, webdriver_status);
             }
             Method resultMethod = c.getMethod("getResult");
-            System.out.println(resultMethod.invoke(wb));
             Method statusMethod = c.getMethod("getTaskStatus");
-            System.out.println(statusMethod.invoke(wb));
+            List<HashMap<String, String>> result = (List<HashMap<String, String>>) resultMethod.invoke(wb);
+            String status = (String) statusMethod.invoke(wb);
+            System.out.println("##"+testJob.job);
+            System.out.println("### 測試案例資訊");
+            System.out.println("* 測試案例描述："+testJob.description);
+            System.out.println("* 測試案例步驟描述："+testJob.testCaseDescription);
+            System.out.println("* 瀏覽器類型："+testJob.webdriverType);
+            System.out.println("* 是否啟用畫面擷圖："+testJob.screenshot);
+            System.out.println("* * * ");
+            System.out.println("### 測試步驟結果");
+            System.out.println("```");
+
+            for (HashMap<String, String> r: result) {
+                System.out.println("* 步驟："+r.get("Step")+" --> 結果："+r.get("Status"));
+            }
+            
+            System.out.println("```");
+            System.out.println("* * * ");
+            System.out.println("執行結果："+status);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
