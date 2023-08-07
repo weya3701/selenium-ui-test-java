bd0fbcb00d43cc78f4ec5d6bdda160fed61159af
Allen Yang <musasi.yang@gmail.com>
2023-08-07 17:01:56 +0800

    feature: 因應新增報告功能，新增DemoWebDriver模組功能 新增： 1) saveResult 2) getResult 3) getTaskStatus 4) setTaskStepStatus 修改： 將動件模組改為有回傳值，回傳值為執行結果。

--- src/main/java/org/example/DemoWebDriver.java
+++ src/main/java/org/example/DemoWebDriver.java
@@ -11,15 +11,15 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.TreeSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.function.Function;
-import static java.util.Map.entry;
 import java.time.Duration;
 
 import org.openqa.selenium.OutputType;
@@ -31,12 +31,29 @@ import java.nio.file.Paths;
 
 public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
 
-    private void saveResult(String result, String params) {
-        this.resultQueue.put("step_result",
-                Map.ofEntries(
-                        entry("step", params),
-                        entry("status", result)
-                ));
+    public void saveResult(String step, String status) {
+        HashMap<String, String> stepStatus = new HashMap<String, String>();
+        stepStatus.put("Step", step);
+        stepStatus.put("Status", status);
+        this.stepsResult.add(stepStatus);
+    }
+
+    public List<HashMap<String, String>> getResult() {
+        return this.stepsResult;
+    }
+
+    public String getTaskStatus() {
+
+        String rsp = this.failed;
+
+        if (this.taskStepStatus) {
+            rsp = this.successful;
+        }
+        return rsp;
+    }
+
+    private void setTaskStepStatus(boolean status) {
+        this.taskStepStatus = this.taskStepStatus && status;
     }
 
     public void getScreenshot(Step step) {
@@ -81,31 +98,39 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
         return by;
     }
 
-    public void switch_tab(Step step) {
+    public String switch_tab(Step step) {
 
         Set<String> wdtabs = new TreeSet<>();
-
         wdtabs = this.webDriver.getWindowHandles();
         this.webDriver.switchTo().window(
                 wdtabs.stream().toList().get(step.tab)
         );
+        setTaskStepStatus(true);
+        return this.successful;
     }
 
     @Override
-    public void open_website(Step step) {
+    public String open_website(Step step) {
+
         this.webDriver.get(step.url);
         setSecondsSleep(step.interval);
+        setTaskStepStatus(true);
+        return this.successful;
     }
 
-    public void open_new_tab(Step step) {
+    public String open_new_tab(Step step) {
 
         JavascriptExecutor executor = (JavascriptExecutor) this.webDriver;
         executor.executeScript("window.open('');");
         setSecondsSleep(step.interval);
+        setTaskStepStatus(true);
+        return this.successful;
     }
 
     @Override
-    public void find_element_and_click(Step step) {
+    public String find_element_and_click(Step step) {
+
+        String rsp = this.successful;
         try {
             By by = getElementBy(step.elementName, step.by);
             Function<WebDriver, WebElement> condition = (WebDriver d) -> d.findElement(by);
@@ -113,25 +138,34 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
                     .until(condition);
             element.click();
             setSecondsSleep(step.interval);
+            setTaskStepStatus(true);
         } catch (Exception ex) {
             ex.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
         }
+        return rsp;
     }
 
     @Override
-    public void find_element_and_click_without_wait(Step step) {
+    public String find_element_and_click_without_wait(Step step) {
+        String rsp = this.successful;
         try {
             By by = getElementBy(step.elementName, step.by);
             this.webDriver.findElement(by).click();
             setSecondsSleep(step.interval);
+            setTaskStepStatus(true);
         } catch (Exception ex) {
             ex.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
         }
+        return rsp;
     }
 
     @Override
-    public void find_element_and_sendkey(Step step) {
-
+    public String find_element_and_sendkey(Step step) {
+        String rsp = this.successful;
         try {
             // Do find_element_and_sendkey
             By by = getElementBy(step.elementName, step.by);
@@ -140,53 +174,78 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
                     .until(condition);
             element.sendKeys(step.key);
             setSecondsSleep(step.interval);
+            setTaskStepStatus(true);
         } catch (Exception ex) {
             ex.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
         }
+        return rsp;
     }
 
     @Override
-    public void find_element_and_sendkey_by_js(Step step) {
-        String script = String.format(
-            """
-            ele = document.evaluate(
-                '%s',
-                document,
-                null,
-                XPathResult.FIRST_ORDERED_NODE_TYPE,
-                null
-            ).singleNodeValue;
-            ele.value='%s'
-            """, step.elementName, step.key
-        );
-        script.replaceAll("'", "\"");
-        script.replaceAll("\"", "'");
-        JavascriptExecutor executor = (JavascriptExecutor) this.webDriver;
-        executor.executeScript(script);
+    public String find_element_and_sendkey_by_js(Step step) {
+        String rsp = this.successful;
+        try {
+            String script = String.format(
+                  """
+                  ele = document.evaluate(
+                      '%s',
+                      document,
+                      null,
+                      XPathResult.FIRST_ORDERED_NODE_TYPE,
+                      null
+                  ).singleNodeValue;
+                  ele.value='%s'
+                  """, step.elementName, step.key
+            );
+            script.replaceAll("'", "\"");
+            script.replaceAll("\"", "'");
+            JavascriptExecutor executor = (JavascriptExecutor) this.webDriver;
+            executor.executeScript(script);
+            setTaskStepStatus(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
+        }
+
+        return rsp;
     }
 
     @Override
-    public void scroll_element_intoview(Step step) {
-        String script = String.format(
-            """
-            ele = document.evaluate(
-                '%s',
-                document,
-                null,
-                XPathResult.FIRST_ORDERED_NODE_TYPE,
-                null
-            ).singleNodeValue;
-            ele.scrollIntoView();
-            """, step.elementName
-        );
-        script.replaceAll("'", "\"");
-        script.replaceAll("\"", "'");
-        JavascriptExecutor executor = (JavascriptExecutor) this.webDriver;
-        executor.executeScript(script);
+    public String scroll_element_intoview(Step step) {
+        String rsp = this.successful;
+        try {
+            String script = String.format(
+                  """
+                  ele = document.evaluate(
+                      '%s',
+                      document,
+                      null,
+                      XPathResult.FIRST_ORDERED_NODE_TYPE,
+                      null
+                  ).singleNodeValue;
+                  ele.scrollIntoView();
+                  """, step.elementName
+            );
+            script.replaceAll("'", "\"");
+            script.replaceAll("\"", "'");
+            JavascriptExecutor executor = (JavascriptExecutor) this.webDriver;
+            executor.executeScript(script);
+            setTaskStepStatus(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
+        }
+
+        return rsp;
     }
 
     @Override
-    public void switch_frame(Step step) {
+    public String switch_frame(Step step) {
+        String rsp = this.successful;
         try {
             boolean isEqual = step.frame.equalsIgnoreCase("parent");
             if (isEqual) {
@@ -194,14 +253,18 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
             } else {
                 this.webDriver.switchTo().frame(step.frame);
             }
+            setTaskStepStatus(true);
         } catch (Exception ex) {
             ex.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
         }
+        return rsp;
     }
 
     @Override
-    public void get_value_to_store(Step step) {
-
+    public String get_value_to_store(Step step) {
+        String rsp = this.successful;
         String store_key = step.storeKey;
         By selectType = getElementBy(step.elementName, step.by);
         String element_value = "";
@@ -212,14 +275,19 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
                     selectType
             ).getText();
             storeValue.put("key", element_value);
+            setTaskStepStatus(true);
         } catch(Exception ex) {
+            rsp = this.failed;
             ex.printStackTrace();
+            setTaskStepStatus(false);
         }
         this.resultQueue.put(store_key, storeValue);
+        return rsp;
     }
 
     @Override
-    public void validation(Step step) {
+    public String validation(Step step) {
+        String rsp = this.successful;
         String result = step.result;
         By selectType = getElementBy(step.elementName, step.by);
         String element_value = "";
@@ -228,19 +296,27 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
             element_value = this.webDriver.findElement(
                     selectType
             ).getText();
+            setTaskStepStatus(true);
         } catch (Exception e) {
             e.printStackTrace();
-        }
+            rsp = this.failed;
+            setTaskStepStatus(false);
+        } finally {
 
-        boolean isEqual = element_value.equalsIgnoreCase(result);
-        if (isEqual) {
-            System.out.println("The value is correct.");
-        } else {
-            System.out.println("The value is incorrect");
+            boolean isEqual = element_value.equalsIgnoreCase(result);
+            if (isEqual) {
+                rsp = this.successful;
+                System.out.println(this.successful);
+            } else {
+                rsp = this.failed;
+                System.out.println(this.failed);
+            }
         }
+        return rsp;
     }
 
-    public void get_regex_value_to_store(Step step) {
+    public String get_regex_value_to_store(Step step) {
+        String rsp = this.successful;
         HashMap<String, String> storeValue = new HashMap<>();
         String element_value = "";
         Pattern pattern = Pattern.compile(step.pattern);
@@ -253,13 +329,18 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
                 element_value = matcher.group();
                 storeValue.put("key", element_value);
             }
+            setTaskStepStatus(true);
         } catch(Exception ex) {
             ex.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
         }
+
+        return rsp;
     }
 
-    public void find_element_and_sendkey_from_store(Step step) {
-
+    public String find_element_and_sendkey_from_store(Step step) {
+        String rsp = this.successful;
         String store_key = step.storeKey;
         HashMap<String, String> storeValue = new HashMap<>();
         storeValue = (HashMap<String, String>) this.resultQueue.get(store_key);
@@ -271,13 +352,19 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
                     .until(condition);
             element.sendKeys(storeValue.get("key"));
             setSecondsSleep(step.interval);
+            setTaskStepStatus(true);
         } catch (Exception ex) {
             ex.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
         }
+
+        return rsp;
     }
 
-    public void validation_count(Step step) {
+    public String validation_count(Step step) {
 
+        String rsp = this.successful;
         Pattern pattern = Pattern.compile(step.pattern);
         Integer total = 0;
         HashMap<String, Integer> resultPattern = new HashMap<>();
@@ -292,13 +379,19 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
         }
 
         if (resultPattern.size() == total) {
-            System.out.println("Successful.");
+            System.out.println(this.successful);
+            setTaskStepStatus(true);
         } else {
-            System.out.println("Failed.");
+            rsp = this.failed;
+            System.out.println(this.failed);
+            setTaskStepStatus(false);
         }
+
+        return rsp;
     }
 
-    public void find_element_and_hover(Step step) {
+    public String find_element_and_hover(Step step) {
+        String rsp = this.successful;
         By selectBy = getElementBy(step.elementName, step.by);
         try {
             this.actions.moveToElement(
@@ -306,9 +399,14 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
                             selectBy
                     )
             ).perform();
+            setTaskStepStatus(true);
         } catch (Exception e) {
             e.printStackTrace();
+            rsp = this.failed;
+            setTaskStepStatus(false);
         }
+
+        return rsp;
     }
 
     static class WebDriverBuilder {
@@ -323,16 +421,19 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
                     driver = new ChromeDriver(co);
                     break;
                 case "Firefox":
+                    System.setProperty("webdriver.firefox.dirver", executablePath);
                     FirefoxOptions fo = new FirefoxOptions();
                     fo.addArguments(browserOptions);
                     fo.addArguments();
                     driver = new FirefoxDriver(fo);
                     break;
                 case "Edge":
+                    System.setProperty("webdriver.edge.dirver", executablePath);
                     EdgeOptions eo = new EdgeOptions();
                     driver = new EdgeDriver(eo);
                     break;
                 case "Safari":
+                    System.setProperty("webdriver.safari.dirver", executablePath);
                     SafariOptions so = new SafariOptions();
                     driver = new SafariDriver(so);
                     break;
@@ -350,7 +451,6 @@ public class DemoWebDriver extends BaseDriver implements WebAutomationTool {
     private Actions actions;
 
     public DemoWebDriver(String webDriverType, String executablePath, String[] browserOptions) {
-        resultQueue = new HashMap<>();
         webDriver = WebDriverBuilder.getWebDriver(webDriverType, executablePath, browserOptions);
         actions = new Actions(webDriver);
     }
