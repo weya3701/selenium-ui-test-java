05f6e05bbddb568f24c418082a811d76aa5b234b
Allen Yang <musasi.yang@gmail.com>
2023-08-07 17:07:10 +0800

    feature: 因應報表功能新增取得result行為

--- src/main/java/org/example/TaskRunner.java
+++ src/main/java/org/example/TaskRunner.java
@@ -8,16 +8,19 @@ public class TaskRunner {
 
         Class c = DemoWebDriver.class;
         Constructor con = c.getDeclaredConstructors()[0];
-
+        String webdriver_status = "";
         try {
             Object wb = con.newInstance(testJob.getWebdriverType(), testJob.getWebdriverPath(), testJob.getOptions());
             for (Step step: Arrays.stream(testJob.steps).toList()) {
-                Method method = c.getMethod(step.module, step.getClass());
-                method.invoke(wb, step);
-                System.out.println(step.desc);
-                method = c.getMethod("getScreenshot", step.getClass());
-                method.invoke(wb, step);
+                Method taskMethod = c.getMethod(step.module, step.getClass());
+                webdriver_status = (String) taskMethod.invoke(wb, step);
+                Method saveMethod = c.getMethod("saveResult", String.class, String.class);
+                saveMethod.invoke(wb, step.desc, webdriver_status);
             }
+            Method resultMethod = c.getMethod("getResult");
+            System.out.println(resultMethod.invoke(wb));
+            Method statusMethod = c.getMethod("getTaskStatus");
+            System.out.println(statusMethod.invoke(wb));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
