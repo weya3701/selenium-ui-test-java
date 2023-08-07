3e7b9d6fc03d37f8cc8e3e6a1a868ed6ae769c7c
Allen Yang <musasi.yang@gmail.com>
2023-08-07 16:58:23 +0800

    feature: 修改WebDriver模組
    新增資料結構：
    1) resultQueue -> HashMap<String, HashMap<String, String>>
    2) stepResult -> List<HashMap<String, String>>
    3) taskStepStatus -> boolean

--- src/main/java/org/example/BaseDriver.java
+++ src/main/java/org/example/BaseDriver.java
@@ -1,7 +1,14 @@
 package org.example;
 
-import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 abstract class BaseDriver {
-    Map<String, Map> resultQueue;
+    HashMap<String, HashMap<String, String>> resultQueue;
+    List<HashMap<String, String>> stepsResult = new ArrayList<HashMap<String, String>>();
+    boolean taskStepStatus = true;
+    String successful = "執行成功";
+    String failed = "執行失敗";
+
 }
