098eec01551ae20413e8f6435cb4da69522c1f23
Allen Yang <musasi.yang@gmail.com>
2023-08-07 17:02:47 +0800

    chore: 移除無法代碼

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Class c = DemoWebDriver.class;
-
-        Constructor con = c.getDeclaredConstructors()[0];
-
         TaskParser tp = new TaskParser(args[0]);
         TestJob tj = tp.getTestJob();
         new TaskRunner(tj);
